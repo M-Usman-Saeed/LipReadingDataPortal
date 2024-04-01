2bcdff3fadcb168ce63996698435d4a54fb71058
M-Usman-Saeed <m.usman.saeed20@gmail.com>
2024-04-01 04:01:25 +0100

    created models for LipReadingDataset

--- lipReadingDataset/models.py
+++ lipReadingDataset/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class TextData(models.Model):
+    GENDERS = [
+        ('M', 'Male'),
+        ('F', 'Female')
+    ]
+    text = models.TextField()
+    gender = models.CharField(max_length=1, choices=GENDERS)
+    race = models.CharField(max_length=50)
+
+class VideoData(models.Model):
+    text_id = models.OneToOneField(TextData, on_delete=models.CASCADE, primary_key=True)
+    video_link = models.URLField()
+    duration = models.DurationField()
+
+class WordDetails(models.Model):
+    text_id = models.ForeignKey(TextData, on_delete=models.SET_DEFAULT)
+    word = models.CharField(max_length=50)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
