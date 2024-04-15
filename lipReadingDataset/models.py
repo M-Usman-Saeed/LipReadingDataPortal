9d86d4cff3fdc19364a7688dfe4b19498565b29f
M-Usman-Saeed <m.usman.saeed20@gmail.com>
2024-04-15 05:37:18 +0100

    Migrated to New DB structure

--- lipReadingDataset/models.py
+++ lipReadingDataset/models.py
@@ -3,21 +3,24 @@ from django.db import models
 # Create your models here.
 
 class TextData(models.Model):
-    GENDERS = [
-        ('M', 'Male'),
-        ('F', 'Female')
-    ]
     text = models.TextField()
-    gender = models.CharField(max_length=1, choices=GENDERS)
-    race = models.CharField(max_length=50)
+    video_link = models.URLField(max_length=200, null=True)
+    video_duration = models.DecimalField(max_digits=10, decimal_places=3, null=True)
 
-class VideoData(models.Model):
-    text_id = models.OneToOneField(TextData, on_delete=models.CASCADE, primary_key=True)
-    video_link = models.URLField()
-    duration = models.DurationField()
+class PartOfSpeach(models.Model):
+    pos = models.CharField(max_length=20)
 
-class WordDetails(models.Model):
+class WordDetail(models.Model):
     text_id = models.ForeignKey(TextData, on_delete=models.DO_NOTHING)
     word = models.CharField(max_length=50)
     start_time = models.TimeField()
     end_time = models.TimeField()
+    word_duration = models.DecimalField(max_digits=10, decimal_places=3, null=True)
+    pos_id = models.OneToOneField(PartOfSpeach, on_delete=models.CASCADE)
+    difficulty = models.CharField(max_length=7)
+    positive = models.BooleanField()
+    negative = models.BooleanField()
+
+class Homephone(models.Model):
+    word_id = models.ForeignKey(WordDetail, on_delete=models.DO_NOTHING)
+    homophone = models.CharField(max_length = 50)
