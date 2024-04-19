a7965e68e59689ccc7d0c47a84710411e4728144
M-Usman-Saeed <m.usman.saeed20@gmail.com>
2024-04-19 07:53:02 +0100

    creating basic url and home page

--- lipReadingDataset/views.py
+++ lipReadingDataset/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from lipReadingDataset.models import TextData
 
 # Create your views here.
+
+def say_hello(request):
+    return render(request, 'home.html', {'name':'Usman'})
